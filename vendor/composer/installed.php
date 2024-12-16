c49ca3a31adda27a138a5aa047866bbae5aa5d99
Meriem Mabrouk <mabrouk.mariem94@gmail.com>
2024-12-16 10:19:18 +0100

    Votre message de commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'meriem/crud_contacts',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6cfe7665275ee550da0f85061dcf6ef776abd4e1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'meriem/crud_contacts' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6cfe7665275ee550da0f85061dcf6ef776abd4e1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
